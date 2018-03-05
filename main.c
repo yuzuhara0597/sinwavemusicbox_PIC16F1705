5e9dffce9391a2dbaaf32d231d389f062f889398
yuzuhara0597 <34590386+yuzuhara0597@users.noreply.github.com>
2018-03-05 22:01:43 +0900

    Update main.c

--- main.c
+++ main.c
@@ -398,7 +398,7 @@ void haruwa_pastel(void){
     note(0,8,6);note(6,8,5);                                                                        //たく
     note(5,8,5);note(5,8,5);note(5,8,5);note(5,8,5);note(6,8,5);note(5,8,5);note(4,8,5);note(3,8,5);//さんのしろいはな
     note(4,18,5);note(4,16,5);note(14,8,5);note(5,8,5);note(6,8,5);note(0,4,6);note(4,8,5);         //きいろいはな　し
-    note(5,4,5);note(5,8,5);note(5,8,5);note(5,8,5);note(6,8,5);note(6,8,5);note(0,8,6);            //がつをうめつく
+    note(5,4,5);note(5,8,5);note(5,8,5);note(5,8,5);note(6,8,5);note(0,8,6);note(2,8,6);            //がつをうめつく
     note(1,2,6);rest(4);                                                                            //す
     
     note(2,8,5);note(3,8,5);                                                                        //のは
